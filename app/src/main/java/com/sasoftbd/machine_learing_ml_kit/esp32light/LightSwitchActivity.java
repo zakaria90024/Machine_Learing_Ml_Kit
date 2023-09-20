4c5b7f68520f8b3825483aeb9a57cc9bd14a4ce5
Zakaria <zakaria112203@gmail.com>
2023-09-21 01:31:40 +0600

    Fan on/off from image Esp32 light on/off wifi added fix plbm 2

--- app/src/main/java/com/sasoftbd/machine_learing_ml_kit/esp32light/LightSwitchActivity.java
+++ app/src/main/java/com/sasoftbd/machine_learing_ml_kit/esp32light/LightSwitchActivity.java
@@ -2,8 +2,11 @@ package com.sasoftbd.machine_learing_ml_kit.esp32light;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.sasoftbd.machine_learing_ml_kit.R;
 
@@ -15,6 +18,7 @@ public class LightSwitchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_light_switch);
         WebView myWebView = (WebView) findViewById(R.id.webview);
         myWebView.loadUrl("http://192.168.4.1/L");
+        myWebView.setWebViewClient(new WebViewClient());
 
     }
 }
